d710343c47ea3fded68ade4dc1cc445adb8a8621
Abhimanyu <151956314+Abhimanyu224462@users.noreply.github.com>
2024-12-18 18:12:14 +0530

    signup completed

--- src/app/shared/components/signup/signup.component.ts
+++ src/app/shared/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { timer, interval, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-signup',
@@ -12,18 +14,24 @@ export class SignupComponent {
 enteredotp!: any;
   enteredotp1: any;
 
+
+
   getotp() {
 
     this.otp = Math.floor(1000 + Math.random() * 9000);
     console.log("OTP Recieved:", this.otp)
     alert(this.otp)
+    this.timer()
 
   }
 
   //mobile verified and tick
-  //timer
+  //timer 
   //validation
   //disabled button
+
+  verifyok: boolean = true;
+  verifyok1:boolean = false;
 
   verifyotp() {
     this.verify = this.enteredotp
@@ -31,7 +39,9 @@ console.log("verify entered", this.verify)
 
     if(this.verify == this.otp){
       console.log("otp for match", this.otp)
-      alert("otp verified successfully")
+      this.verifyok = false
+      this.verifyok1 = true
+      // alert("otp verified successfully")
     } else {
       alert("Wrong OTP Entered")
     }
@@ -39,6 +49,26 @@ console.log("verify entered", this.verify)
   }
 
 
+ngOnInit(){
+  // this.timer()
+}
+
+ timer(){
+  // timer(10,1000).subscribe(n => console.log('timer', n));
+  this.sub = interval(1000).subscribe(n => {
+    this.interval = 10 - n 
+    console.log('interval', this.interval)
+    if(this.interval == 0){
+      this.sub.unsubscribe()
+    }
+  }
+);
+  
+ }
+
+ interval!:number
+ 
+ sub:Subscription = new Subscription()
 
 
 
